5c07d5083fc8ef3cbe127ad88af20d1554d70ba3
shota27shota <136681041+shota27shota@users.noreply.github.com>
2023-08-03 16:13:22 +0900

    編集１

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,4 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin_all_from "app/javascript/items", under: "items"
 pin "card", to: "card.js"
+pin "price", to: "price.js"
